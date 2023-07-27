a73eccbec2ce711ffc3060eca3156dc9c8d9f26b
Rinkit Adhana <therinkit@gmail.com>
2023-07-27 17:40:44 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+
+import java.sql.Time;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Date now = new Date();
+        System.out.println(now);
+
     }
 }
+
+
+
