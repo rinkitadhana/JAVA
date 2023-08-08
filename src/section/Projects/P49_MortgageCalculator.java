9800b1982fa1534afe385a4160e83eaf26d89073
Rinkit Adhana <therinkit@gmail.com>
2023-08-08 20:59:12 +0530

    up

--- src/section/Projects/P49_MortgageCalculator.java
+++ src/section/Projects/P49_MortgageCalculator.java
@@ -1,40 +1,66 @@
 package section.Projects;
 
+import java.security.Principal;
 import java.text.NumberFormat;
 import java.util.Scanner;
-
-public class P49_MortgageCalculator {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double principal =0;
-        double annualInterestRate = 0;
-        double periodInMonth = 0;
-        double interestRate = 0;
-        while (true){
+class Mortgage {
+    Scanner input = new Scanner(System.in);
+    private double principal;
+    private double periodInMonth;
+    private double interestRate;
+    public String mortgageCalculate() {
+        double mortgage = principal * interestRate * (Math.pow(1 + interestRate, periodInMonth) / (Math.pow(1 + interestRate, periodInMonth) - 1));
+        return NumberFormat.getCurrencyInstance().format(mortgage);
+    }
+    public double setPrincipal() {
+        while (true) {
             System.out.print("Principal(1k-1M): ");
             principal = input.nextDouble();
-            if (principal>=1000&&principal<=1000000)
+            if (principal >= 1000 && principal <= 1000000)
                 break;
         }
-        while(true){
+        return principal;
+    }
+    public double setInterestRate() {
+        while (true) {
             System.out.print("Annual Interest Rate: ");
-            annualInterestRate = input.nextDouble();
-            if (annualInterestRate>=1&&annualInterestRate<=30) {
-                interestRate = annualInterestRate / 1200;
+            double annualInterestRate = input.nextDouble();
+            if (annualInterestRate >= 1 && annualInterestRate <= 30) {
+                interestRate = annualInterestRate/1200;
                 break;
             }
         }
-        while(true){
+        return interestRate;
+    }
+    public double setPeriodInMonth() {
+        while (true) {
             System.out.print("Period (Years): ");
             int period = input.nextInt();
-            if (period>=1&&period<=30) {
+            if (period >= 1 && period <= 30) {
                 periodInMonth = period * 12;
                 break;
             }
-
         }
-        double mortgage =  principal * interestRate*(Math.pow(1+interestRate,periodInMonth)/(Math.pow(1+interestRate,periodInMonth)-1));
-        String result = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: "+ result);
+        return periodInMonth;
+    }
+    private double getPrincipal() {
+        return principal;
+    }
+    private double getPeriodInMonth() {
+        return periodInMonth;
+    }
+    private double getInterestRate() {
+        return  interestRate;
     }
 }
+public class P49_MortgageCalculator {
+    public static void main(String[] args) {
+        var mortgage = new Mortgage();
+         mortgage.setPrincipal();
+         mortgage.setInterestRate();
+         mortgage.setPeriodInMonth();
+        String result = mortgage.mortgageCalculate();
+        System.out.println(result);
+    }
+}
+
--- src/section/Topics/T15_OOPS_Car.java
+++ src/section/Topics/T15_OOPS_Car.java
@@ -64,3 +64,21 @@ public class T15_OOPS_Car {
                 (convertable ? "Convertable": "Non Convertable"));
     }
 }
+ class T16_CarRunner {
+    public static void main(String[] args) {
+        T15_OOPS_Car car = new T15_OOPS_Car();
+
+        car.setMake("honda");
+        car.setModel("T-45");
+        car.setColor("Pink");
+        car.setDoors(3);
+        car.setConvertable(false);
+        System.out.println("Make = "+car.getMake());
+        System.out.println("Model = "+car.getModel());
+        System.out.println("Color = "+car.getColor());
+        System.out.println("Doors = "+car.getDoors());
+
+        car.describeCar();
+    }
+}
+
