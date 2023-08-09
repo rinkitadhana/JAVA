d0f8a03f4f1def9f35442bfaf900d05376415c38
Rinkit Adhana <therinkit@gmail.com>
2023-08-10 00:36:37 +0530

    up

--- src/section/Projects/P53_MoshMortgageUsingOOps.java
+++ src/section/Projects/P53_MoshMortgageUsingOOps.java
@@ -18,25 +18,12 @@ class Console{
         }
         return value;
     }
-
 }
-public class P53_MoshMortgageUsingOOps {
-    final static byte MONTHS_IN_YEAR = 12;
-    final static byte PERCENT = 100;
-
-    public static double calculateMortgage(int principal,float annualInterest,byte years){
-        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
-        float months = years*MONTHS_IN_YEAR;
-        return principal * monthlyInterest * (Math.pow(1 +monthlyInterest , months) / (Math.pow(1 + monthlyInterest, months) - 1));
-    }
-    public static double calculateBalance(int principal,float annualInterest,byte years,short numberOfPayments){
-
-        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
-        float months = years*MONTHS_IN_YEAR;
-        return principal* (Math.pow(1+monthlyInterest,numberOfPayments) - Math.pow(1+monthlyInterest,months))/(Math.pow(1+monthlyInterest,numberOfPayments)-1);
-    }
+class MortgageReport{
+    private static MortgageCalculator calculate;
     public static void printMortgage(int principal,float annualInterest, byte years){
-        double mortgage = calculateMortgage(principal, annualInterest, years);
+         calculate = new MortgageCalculator(principal,annualInterest,years);
+        double mortgage = calculate.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
@@ -47,16 +34,48 @@ public class P53_MoshMortgageUsingOOps {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
-        for(short month =1; month<=years*MONTHS_IN_YEAR; month++){
-            System.out.println(NumberFormat.getCurrencyInstance().format(calculateBalance(principal,annualInterest,years,month)));
+        for(short month =1; month<=years*MortgageCalculator.MONTHS_IN_YEAR; month++){
+            System.out.println(NumberFormat.getCurrencyInstance().format(calculate.calculateBalance(month)));
         }
     }
+}
+class MortgageCalculator{
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateMortgage(){
+        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        float months = years*MONTHS_IN_YEAR;
+        return principal * monthlyInterest * (Math.pow(1 +monthlyInterest , months) / (Math.pow(1 + monthlyInterest, months) - 1));
+    }
+    public  double calculateBalance(short numberOfPayments){
+
+        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        float months = years*MONTHS_IN_YEAR;
+        return principal* (Math.pow(1+monthlyInterest,numberOfPayments) - Math.pow(1+monthlyInterest,months))/(Math.pow(1+monthlyInterest,numberOfPayments)-1);
+    }
+
+}
+public class P53_MoshMortgageUsingOOps {
+
+
+
+
     public static void main(String[] args) {
         int principal = (int) Console.readNumber("Principal", 1_000, 1_000_000);
         float annualInterest = (float) Console.readNumber("Annual Interest",1,30);
         byte years = (byte) Console.readNumber("Years",1,30);
-        printMortgage(principal,annualInterest,years);
-        printPaymentSchedule(principal,annualInterest,years);}
+        MortgageReport.printMortgage(principal,annualInterest,years);
+        MortgageReport.printPaymentSchedule(principal,annualInterest,years);}
 
 }
 
