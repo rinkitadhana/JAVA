00d055a4b480aea8873a6677ef97e868011e827a
Rinkit Adhana <therinkit@gmail.com>
2023-08-21 01:38:28 +0530

    e

--- src/Age.java
+++ src/Age.java
@@ -11,7 +11,7 @@ public class Age {
     public int month2;
     public int year1;
     public int year2;
-
+    private final int[] numberOfDaysInAMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
     public int AgeInYears() {
         if(year2 > year1 && year1>0) {
             if(month2 > month1 || month2 == month1 && day2 >= day1)
@@ -25,19 +25,31 @@ public class Age {
     }
     public int AgeInMonths() {
         if(month2 > month1)
-            ageInMonths = month2 - month1;
+            ageInMonths = month2 - month1 - 1;
         else
             ageInMonths = 12 - (month1 - month2);
 
         return ageInMonths;
     }
     public int AgeInDays() {
-        if(day2 > day1)
-            ageInDays = day2 - day1;
-        else
-            ageInDays = 30 - (day1 - day2);
+        if(year1%4==0 || year1%100==0 && year1%400==0) {
+            if(day2 > day1)
+                ageInDays = day2 - day1;
+            else
+            if(month2 == 2){
+                ageInDays = numberOfDaysInAMonth[month2-1] + 1 - (day1 - day2);
+            }
 
+        }
+        else {
+            if(day2 > day1)
+                ageInDays = day2 - day1;
+            else
+                ageInDays =  numberOfDaysInAMonth[month2-1] - (day1 - day2);
+        }
         return ageInDays;
+
+
     }
     public int TotalDays() {
 
@@ -47,7 +59,7 @@ public class Age {
             }
             year1++;
         }
-        return ageInYears*365+ageInMonths*30+extraDays;
+        return ageInYears*365 + ageInMonths*30 + extraDays + ageInDays;
     }
 
 }
---
+++ src/section/Top10/P2_TikTakToe.java
@@ -0,0 +1,5 @@
+package section.Top10;
+
+public class P2_TikTakToe {
+
+}
