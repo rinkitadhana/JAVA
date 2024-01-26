af4619e2d99407a15f5238c7cfa10eddbd256ae7
Rinkit Adhana <therinkit@gmail.com>
2024-01-26 14:37:51 +0530

    update

---
+++ src/section/rough/Revise.java
@@ -0,0 +1,24 @@
+package section.rough;
+
+public class Revise {
+    public static void main(String[] args) {
+        System.out.println("Rinkit Adhana");
+        var rinkit = new Rinkit();
+        System.out.println(rinkit.rinkitt());
+        var fiv = new Halwa();
+        fiv.rinkitttt();
+    }
+}
+class Rinkit{
+    int rinkiit= 10;
+    public int rinkitt(){
+        return (rinkiit + 10);
+    }
+}
+class Halwa{
+    int tin = 20;
+    public void rinkitttt(){
+        System.out.println("Hello my name is rinkit ");
+        System.out.println(10+tin);
+    }
+}
