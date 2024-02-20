24e9a66e71b81a356fc7dc9e60d4d1734f5c10c7
Rinkit Adhana <therinkit@gmail.com>
2024-02-20 16:22:22 +0530

    a

--- src/section/Topics/T21_Array.java
+++ src/section/Topics/T21_Array.java
@@ -4,23 +4,25 @@ import java.util.Scanner;
 
 public class T21_Array {
     public static void main(String[] args) {
-        int numbers[] = {1, 2, 3, 5, 6};
+        int[] numbers = {1, 2, 3, 5, 6};
         System.out.println(numbers[2]);
-        int number[] = new int[10];
+        int[] number = new int[10];
         number[0] = 10;
         System.out.println(number[0]);
-        int rinkit[];
+        int[] rinkit;
         rinkit = new int[10];
         rinkit[0] = 19;
-        String rin[];
+        String[] rin;
         rin = new String[10];
         rin[2] = "rinkit";
-        int a[], b[], c;//c is wrong.
+        int[] a; //c is wrong.
+        int[] b;
+        int c;
         a = new int[40];
         a[1] = 19;
         b = new int[29];
         System.out.println(a.length);
-        int t[] = new int[10];
+        int[] t = new int[10];
         t[0] = 0;
         t[1] = 1;
         t[2] = 2;
@@ -28,8 +30,6 @@ public class T21_Array {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < t.length; i++) {
             System.out.println("array" + i + "value" + t[i]);
-
-
             int[][] num = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
             System.out.println(Arrays.deepToString(num));
             int[][][] r = new int[2][3][4];
